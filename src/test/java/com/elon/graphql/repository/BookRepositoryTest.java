1f42014eee2255d98893033a503fead752bc6f8c
elon.kwon <elon.kwon@kakaocorp.com>
2018-09-06 18:44:22 +0900

    Implement GraphQLMutationResolver for Book

--- src/test/java/com/elon/graphql/repository/BookRepositoryTest.java
+++ src/test/java/com/elon/graphql/repository/BookRepositoryTest.java
@@ -1,11 +1,17 @@
 package com.elon.graphql.repository;
 
+import com.elon.graphql.model.Book;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,6 +22,14 @@ public class BookRepositoryTest {
     @Test
     public void findAll() {
         Assert.assertNotNull(bookRepository.findAll());
+    }
+
+    @Test
+    public void findOneBook() {
+        assertThat(bookRepository.findOneBook((long) 1),
+                anyOf(instanceOf(Book.class), nullValue()));
+        assertThat(bookRepository.findOneBook((long) 10),
+                anyOf(instanceOf(Book.class), nullValue()));
     }
 
 }
